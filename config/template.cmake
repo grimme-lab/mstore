f27233d8e8dd2fc574cb8f3253d1a4fbb1dc6ffe
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-06-29 11:13:09 +0200

    Allow finding of Fortran dependencies in CMake and meson

--- config/template.cmake
+++ config/template.cmake
@@ -4,6 +4,7 @@ if(NOT TARGET "@PROJECT_NAME@::@PROJECT_NAME@")
   include("${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake")
 
   include(CMakeFindDependencyMacro)
+  list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/Modules")
 
   if(NOT TARGET "mctc-lib::mctc-lib")
     find_dependency("mctc-lib")
